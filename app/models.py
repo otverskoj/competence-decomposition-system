50a1bb1497108d1aeee16c7517e32ad068e1ba91
Oleg Tverskoy <otverskoj@gmail.com>
2022-05-25 16:48:52 +0600

    Add pydantic models for input/output JSON

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from typing import Iterable
+from pydantic import BaseModel
+
+
+class Competence(BaseModel):
+    competence: str
+    top_n: int
+
+
+class Keyword(BaseModel):
+    word: str
+    weight: float
+
+
+class TopicData(BaseModel):
+    topic: int
+    keywords: Iterable[Keyword]
+
+
+class CompetenceDecomposition(BaseModel):
+    competence: str
+    top_n: int
+    topic_indices: Iterable[int]
+    topic_keywords: Iterable[TopicData]
